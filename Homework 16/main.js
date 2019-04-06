436d3e4e84180031b6ffa482546bd106fc06d945
Vitalii <vitalii@Vitaliis-MacBook-Air.local>
2019-04-06 22:40:32 +0300

    Add homework 16 part 2

--- Homework 16/main.js
+++ Homework 16/main.js
@@ -2,20 +2,15 @@ window.onload = function(){
 
     let cinemaItems = document.querySelectorAll('.cinema__item');
     
-        //From Node.list to Array
+        // From Node.list to Array
     let cinemaItemsArr = Array.from(cinemaItems);
-    
-        //Second variant from Node.list to Array
-    // cinemaItems.forEach((elem, pos ,arr) => {
-    //     cinemaItemsArr[pos] = elem;
-    // })
-    
-        //Clean empty data
+
+        // Clean empty data
     let filtredArr = cinemaItemsArr.filter((elem) => {
         return (elem.children[0].textContent != '') && (elem.children[1].textContent != '');
     });
     
-        //Display films and cost
+        // Display films and cost
     let res = filtredArr.reduce((prev, elem) => {
         console.log(`
             Film: ${elem.children[0].textContent}, Cost: ${elem.children[1].textContent};
@@ -23,8 +18,21 @@ window.onload = function(){
         return prev += parseInt(elem.children[1].textContent);
     }, 0);
     
-        //Display total cost
+        // Display total cost
     console.log(`
-        Total cost = ${res};
+        Total cost = ${res}$;
     `);
+
+    // Second exercise
+
+    let match = 'hello';
+    let arr = ["1234hello", "hellohd;lfmg", "45yu89egrhellowegojih", "dfhghello", ",bnsdklb"];
+    
+    let resIndex = arr.reduce((prev, elem, pos, arr) => {
+        prev[pos] = elem.indexOf(match);
+        return prev;
+    }, []);
+    console.log('Result of searching: ', resIndex);
+
+
 }
