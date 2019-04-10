862f471a1c8fbe1497dd70d859adba9d9ef96eea
Vitalii <vitalii@Vitaliis-MacBook-Air.local>
2019-04-10 14:35:04 +0300

    make changes in homework 12

--- Homework 12/main_2.js
+++ Homework 12/main_2.js
@@ -2,9 +2,9 @@ let block_1 = document.querySelector('.block-1'),
     block_2 = document.querySelector('.block-2'),
     block_3 = document.querySelector('.block-3'),
     blocks = document.querySelectorAll('div');
-block_1.style.cssText = "margin: 20px; width: 100px; height: 100px; border: 1px solid black; background-color: blue; position: relative; top: 0; left: 0; transition: 1s;";
-block_2.style.cssText = "margin: 20px; width: 100px; height: 100px; border: 1px solid black; background-color: green; position: relative; top: 0; left: 0; transition: 1s;";
-block_3.style.cssText = "margin: 20px; width: 100px; height: 100px; border: 1px solid black; background-color: orange; position: relative; top: 0; left: 0; transition: 1s;";
+block_1.style.cssText = "width: 100px; height: 100px; border: 1px solid black; background-color: blue; position: absolute; top: 0; left: 0; transition: 1s;";
+block_2.style.cssText = "width: 100px; height: 100px; border: 1px solid black; background-color: green; position: absolute; top: 100px; left: 0; transition: 1s;";
+block_3.style.cssText = "width: 100px; height: 100px; border: 1px solid black; background-color: orange; position: absolute; top: 200px; left: 0; transition: 1s;";
 
 function getRandColor(elemNumber){
     let red = Math.floor(Math.random()*256),
@@ -13,10 +13,11 @@ function getRandColor(elemNumber){
         blocks[elemNumber].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 setInterval(() => {
-    let randTop = Math.floor(Math.random()*400 + 1),
-        randleft = Math.floor(Math.random()*700 + 1),
+    let randTop = Math.floor(Math.random()*(window.innerHeight - 100)),
+        randleft = Math.floor(Math.random()*(document.documentElement.clientWidth - 100)),
         randElm = Math.floor(Math.random()*3);
+
     blocks[randElm].style.top = `${randTop}px`;
     blocks[randElm].style.left = `${randleft}px`;
     getRandColor(randElm);
-}, 2000);
+}, 500);
