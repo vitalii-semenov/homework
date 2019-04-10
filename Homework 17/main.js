ddab58a3bc5570ab83a3e663ded5fb4a07ea9306
Vitalii <vitalii@Vitaliis-MacBook-Air.local>
2019-04-10 14:04:45 +0300

    add homework 17 part 2 and some changes in part 1

--- Homework 17/main.js
+++ Homework 17/main.js
@@ -13,28 +13,50 @@ window.onload = function(){
         xhr.send(formData);
         result.innerText = xhr.status + ' ' + xhr.statusText;
     }
-    
-    function getName(ev){
-        let arr = ev.target.value.split('');
 
-        for (let i = 0; i <= arr.length; i++){
-            if (!isNaN(arr[i])){
+    //  Exercise 2
+    function getReq(){
+        let req1 = new XMLHttpRequest(),
+            req2 = new XMLHttpRequest();
+        req1.open('GET' , 'data.json', false);
+        req2.open('GET' , 'data2.json', false);
+
+        req1.send();
+        req2.send();
+
+        arr1 = JSON.parse(req1.responseText);
+        arr2 = JSON.parse(req2.responseText);
+        for (key in arr1){
+            for (key2 in arr2){
+                arr1[key] = arr1[key].concat(arr2[key2]);
+            }
+        }
+        return arr1;
+    }
+    let sumArr = getReq();
+    console.log(sumArr);
+    //------------------------------------------------------------
+
+    function getName(ev){
+        let req = ev.target.value.split('');
+        result.innerText = null;
+
+        for (let i = 0; i <= req.length; i++){
+            if (!isNaN(req[i])){
                 afterName.style.display = 'inline-block';
                 sent.disabled = true;
                 ev.target.classList.add('errorName');
-                result.innerText = null;
                 return;
             } else {
                 ev.target.classList.remove('errorName');
                 afterName.style.display = 'none';
                 sent.disabled = false;
-                result.innerText = null;
             }
         }
 
             //  With Map doesn't work
         //
-        // toName.value.split('').map((element, pos, arr) => {
+        // toName.value.split('').map((element, pos, req) => {
         //     if (!isNaN(element)){
         //         console.log(pos)
         //         afterName.style.display = 'inline-block';
