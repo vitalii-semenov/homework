c1980894a17774d86678dbf14b5f199d2dd4990d
Vitalii <vitalii@Vitaliis-MacBook-Air.local>
2019-04-18 23:55:38 +0300

    Add homework 19 v2.0(with rendering a table)

--- Homework 19/main.js
+++ Homework 19/main.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', () => {
+
     let gitRepo = document.querySelector('.git-repo');
 
     function getFromGit(url) {
@@ -9,7 +10,7 @@ window.onload = function() {
             listRep.addEventListener('readystatechange', function() {
                 if (this.readyState != 4) return;
                 if (this.status == 200) {
-                    resolve(this.response);
+                    resolve(JSON.parse(this.response));
                 } 
                 reject(this.status);
             })
@@ -22,24 +23,19 @@ window.onload = function() {
     getFromGit('https://api.github.com/orgs/hillel-front-end/repos')
         .then(response => {
             let hillelRepos = {};
-            let chain = Promise.resolve();
-            JSON.parse(response).forEach(elem => {
-                chain = chain
-                    .then(() => getFromGit(`https://api.github.com/repos/hillel-front-end/${elem.name}/languages`))
-                    .then(JSON.parse)
+            let res = response.reduce((prev, elem) => {
+                return getFromGit(`https://api.github.com/repos/hillel-front-end/${elem.name}/languages`)
                     .then(resolve => {
-                        let langList = '';
-                        for (lang in resolve){
-                            langList += lang + ', ';
-                        }
+                        let langList =  Object.keys(resolve).join(', ');
+                        
                         hillelRepos[elem.name] = {
-                        default_branch: elem.default_branch,
-                        last_upd: elem.updated_at,
-                        languages: langList
+                            default_branch: elem.default_branch,
+                            last_upd: elem.updated_at,
+                            languages: langList
                         }
                     })
-            });
-            chain.then(() => {
+            }, Promise.resolve());
+            res.then(() => {
                 renderRes(hillelRepos)
             })
         })
@@ -48,16 +44,33 @@ window.onload = function() {
         })
 
     function renderRes(obj) {
-        gitRepo.style.display = 'block';
+        let table = document.createElement('table'),
+            tr = document.createElement('tr'),
+            thName = document.createElement('th'),
+            thBranch = document.createElement('th'),
+            thLastUpdate = document.createElement('th'),
+            thLanguages = document.createElement('th');
+        thName.innerHTML = 'Repository name';
+        thBranch.innerHTML = 'Default branch';
+        thLastUpdate.innerHTML = 'Last update';
+        thLanguages.innerHTML = 'Languages';
+        tr.append(thName, thBranch, thLastUpdate, thLanguages);
+        table.append(tr);
+
         for (let key1 in obj){
-            let a = document.createElement('tr');
-            a.innerHTML = `<td>${key1}</td>`
+            let tr = document.createElement('tr'),
+                tdName = document.createElement('td');
+            tdName.append(key1);
+            tr.append(tdName);
+
             for (let key2 in obj[key1]){
-                let b = document.createElement('td');
-                b.innerHTML = obj[key1][key2];
-                a.appendChild(b);
+                let td = document.createElement('td');
+                td.innerHTML = obj[key1][key2];
+                tr.appendChild(td);
             }
-            gitRepo.appendChild(a);
+
+            table.append(tr);
         }
+        document.querySelector('body').append(table);
     }
-}
+})
